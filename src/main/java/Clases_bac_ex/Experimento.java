c9785cae63fe12d5b5d0cf02090adc984e2f768c
aalvaroo7 <almarrom@myuax.com>
2024-05-04 15:07:45 +0200

    .

--- src/main/java/Clases_bac_ex/Experimento.java
+++ src/main/java/Clases_bac_ex/Experimento.java
@@ -1,4 +1,7 @@
 package Clases_bac_ex;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -36,6 +39,37 @@ public class Experimento {
 
     public void setBacterias(List<Bacteria> bacterias) {
         this.bacterias = bacterias;
+    }
+
+    public void guardarExperimento(String rutaArchivo) {
+        try (PrintWriter out = new PrintWriter(new FileWriter(rutaArchivo))) {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            out.println(sdf.format(fechaInicio));
+            out.println(sdf.format(fechaFin));
+            for (Bacteria bacteria : bacterias) {
+                out.println(bacteria.getNombre() + "," + bacteria.getCantidad());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Experimento abrirExperimento(String rutaArchivo) {
+        try (BufferedReader in = new BufferedReader(new FileReader(rutaArchivo))) {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            Date fechaInicio = sdf.parse(in.readLine());
+            Date fechaFin = sdf.parse(in.readLine());
+            String linea;
+            List<Bacteria> bacterias = new ArrayList<>();
+            while ((linea = in.readLine()) != null) {
+                String[] partes = linea.split(",");
+                bacterias.add(new Bacteria(partes[0], Integer.parseInt(partes[1])));
+            }
+            return new Experimento(fechaInicio, fechaFin, bacterias);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
 
