1e4dd179e046077423e3fb2f8a1ca823d730fa88
aalvaroo7 <almarrom@myuax.com>
2024-05-18 18:17:22 +0200

    Parte 2

---
+++ src/main/java/GUI/Aplicacion.java
@@ -0,0 +1,62 @@
+package GUI;
+import Gestion_experimentos.Experimento;
+import Simulacion.Bacteria;
+import java.util.List;
+
+public class Aplicacion {
+    private Experimento experimentoActual;
+
+    public Aplicacion() {
+        this.experimentoActual = null;
+    }
+
+    public void abrirExperimento(String filePath) {
+        this.experimentoActual = Experimento.cargarExperimento(filePath);
+    }
+
+    public void crearExperimento(String nombre, Date fechaInicio, int duracion, int patronAlimentacion, List<Bacteria> poblacionesBacterias) {
+        this.experimentoActual = new Experimento(nombre, fechaInicio, duracion, patronAlimentacion, poblacionesBacterias);
+    }
+
+    public void crearPoblacion(Bacteria bacteria) {
+        if (this.experimentoActual != null) {
+            this.experimentoActual.addPoblacion(bacteria);
+        }
+    }
+
+    public List<Bacteria> visualizarPoblaciones() {
+        if (this.experimentoActual != null) {
+            return this.experimentoActual.getPoblacionesBacterias();
+        }
+        return null;
+    }
+
+    public void borrarPoblacion(Bacteria bacteria) {
+        if (this.experimentoActual != null) {
+            this.experimentoActual.removePoblacion(bacteria);
+        }
+    }
+
+    public Bacteria verDetallesPoblacion(String nombre) {
+        if (this.experimentoActual != null) {
+            for (Bacteria bacteria : this.experimentoActual.getPoblacionesBacterias()) {
+                if (bacteria.getNombre().equals(nombre)) {
+                    return bacteria;
+                }
+            }
+        }
+        return null;
+    }
+
+    public void simularPoblacion() {
+        if (this.experimentoActual != null) {
+            this.experimentoActual.iniciarExperimento();
+        }
+    }
+
+    public void guardarExperimento(String filePath) {
+        if (this.experimentoActual != null) {
+            this.experimentoActual.guardarExperimento(filePath);
+        }
+    }
+}
