6a743ea9080c044669f93f4fd4e3e0d1821a3762
aalvaroo7 <almarrom@myuax.com>
2024-05-18 19:33:44 +0200

    Parte 2

--- src/main/java/SimulacionMontecarlo/Simulacion.java
+++ src/main/java/SimulacionMontecarlo/Simulacion.java
@@ -3,12 +3,15 @@ package SimulacionMontecarlo;
 import Gestion_experimentos.Experimento;
 import Gestion_experimentos.PlatoCultivo;
 
+import java.util.Random;
+
 public class Simulacion {
     private PlatoCultivo platoCultivo;
     private Experimento experimento;
     private int diaActual;
     private int totalBacteria;
     private int totalFood;
+    Random rand = new Random();
     public Simulacion(PlatoCultivo platoCultivo, Experimento experimento) {
         this.platoCultivo = platoCultivo;
         this.experimento = experimento;
